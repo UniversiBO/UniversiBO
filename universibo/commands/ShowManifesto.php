86b79e57bf03b1c9616a9c51aa12e8ef3f3d1325
Fabrizio Pinto <evaimitico@gmail.com>
2003-09-04 20:06:12 +0000

    *** empty log message ***

--- universibo/commands/ShowManifesto.php
+++ universibo/commands/ShowManifesto.php
@@ -24,80 +24,9 @@ class ShowManifesto extends UniversiboCommand {
 		$template->assign('config_styleSheet', '<link rel="stylesheet" href="tpl/black/style.css" type="text/css">');
 
 
-        $template->assign('manifesto_langTitleImg', 'tpl/black/manifesto_30.gif');
-		$template->assign('manifesto_langQuoteImg', 'tpl/black/galileo_galilei.gif');
 		$template->assign('manifesto_langQuoteAlt', 'Galileo Galilei: Ma sopra tutte le invezioni stupende qual eminenza di mente fu quella di colui che s\'immaginò di trovar modo di comunicare i suoi più reconditi pensieri a qualsivoglia altra persona, benché distante per lunghissimo intervallo di luogo e di tempo?');
-		$template->assign('manifesto_langWhatIsIt', 'Forse l\'avrete banalmente già notato, ma lo scopo di questo sito è
-quello di aiutare una specie animale che da tempo immemorabile s\'inerpica tutte
-le mattine per una salitella ai piedi dei colli Bolognesi... si tratta dello
-studente d\'ingegneria.\n
-\n
-Non si sa cosa li spinga tutti i giorni a compiere queste fatiche... fatto sta
-che mediamente dopo quasi una decina d\'anni abbandona questi luoghi... non si sa
-dove vadano e il più triste dei particolari è che nemmeno loro
-sanno cosa cercare una volta usciti! ...e finiscono solo col chiedersi
-perch&eacute; vi siano entrati.\n
-\n
-Un giorno ero seduto su una panchina a godermi l\'ultimo sole autunnale, mentre
-tentavo di dare un\'interpretazione alle nuvole nel cielo... una
-farfalla... girava per i giardini dell\'Eden proprio ai piedi della collina in
-cui si recavano gli ingegneri, mi si avvicinò, e mi raccontò una
-strana storia...\n
-\n
-Mi disse che un giorno lei, era bruco, era nata in un angolino, dentro una
-strana gabbia con dei misteriosi vetri trasparenti al posto delle sbarre.\n
-Tutti i giorni questo posto veniva frequentato regolarmente da questi strani
-ingegneri, loro la chiamavano aula...\n
-C\'era una persona in particolare più anziana delle altre, sembrava essere
-il loro capo e voleva che gli altri gli portassero rispetto, anche se non sempre
-ci riusciva. Alcuni pretendevano di essere autorevoli, ma tranne qualche
-eccezione, non accorgendosi della contraddizione finivano con essere solo
-autoritari.\n
-Tutti loro si facevano chiamare professori, ma alcuni non erano capaci di
-insegnare le loro conoscenze ...mentre altri erano davvero dei maestri del
-sapere!!\n
-Questa persona tracciava degli strani segni su una superficie nera, e parlava
-...e tutti gli altri lo stavano ad ascoltare ricopiando quegli strani disegni,
-alcuni con aria assonnata, altri pieni di dubbi.\n
-\n
-Per fortuna un giorno lei si addormentò e quando si svegliò gli
-spuntarono due stupende ali variopinte che le permisero di volare via dalla sua
-gabbia.\n
-Era libera! Finalmente libera!\n
-Ed era felice! Finalmente felice, di potersene stare in quel bel giardino
-all\'aria aperta.\n
-Finalmente poteva girare in tutte le direzioni senza farsi influenzare da chi le
-diceva di scegliere la via di destra, sinistra o dritto al centro... se solo voleva
-poteva tornarsene indietro infischiandosene di tutto!! \n
-Ma il fatto che tutti quegli ingegneri se ne stessero lì dentro la
-turbava un po\'.\n
-\n
-A quel punto mi regalò la sua idea: "Perchè non ricopi anche
-tu quegli strani disegni? Fai in modo che tutti possano copiarseli con poca
-fatica, così quei poveri ingegneri se ne potranno andare prima dalla loro
-gabbia e potranno essere come me che volo felice per questo giardino".\n
-\n
-Allora nel colpo di un clock misi in opera la sua idea, con l\'aiuto di alcuni
-amici molto bravi e generosi, ricopiai quegli strani disegni in un luogo
-straordinario, raggiungibile da tutto il mondo e dove ognuno può farsene
-una copia con un click!\n
-\n
-Ebbi la possibilità di parlare con alcuni di questi ingegneri, anche loro
-erano tutti contenti e felici, finalmente avevano a disposizione un po\' di aiuto
-per sollevarli dal loro carico... non capii perchè, ma qualcuno mi disse
-che ora poteva volare.\n
-\n
-In una strana giornata, piovigginava leggermente con il sole che ancora faceva
-capolino tra le nubi, tornai nel giardino per ringraziare la mia amica farfalla...
-girai a destra e a manca, provai a chiamarla, ma mi accorsi che non sapevo
-nemmeno il suo nome.\n
-...non riuscii a trovarla, e stanco mi rimisi a sedere sulla panchina.\n
-Però dopo un po\' mi si presentò una simpatica rondinella, mi disse:
-"Erano giorni che avevo una fame terribile... sai, vengo da un lungo viaggio.
-Per fortuna che è passata da questa parti una farfalla che era tanto
-felice e faceva tanto chiasso che non ho potuto non notarla... davvero un buon
-pasto!".\n');
-		$template->assign('manifesto_langAuthor', 'Brain');
+		$template->assign('manifesto_langWhatIsIt', "Forse l'avrete banalmente già notato, ma lo scopo di questo sito è quello di aiutare una specie animale che da tempo immemorabile s'inerpica tutte le mattine per una salitella ai piedi dei colli Bolognesi... si tratta dello studente d'ingegneria.\n\nNon si sa cosa li spinga tutti i giorni a compiere queste fatiche... fatto sta che mediamente dopo quasi una decina d'anni abbandona questi luoghi... non si sa dove vadano e il più triste dei particolari è che nemmeno loro sanno cosa cercare una volta usciti! ...e finiscono solo col chiedersi perché vi siano entrati.\n\nUn giorno ero seduto su una panchina a godermi l'ultimo sole autunnale, mentre tentavo di dare un'interpretazione alle nuvole nel cielo... una farfalla... girava per i giardini dell'Eden proprio ai piedi della collina in cui si recavano gli ingegneri, mi si avvicinò, e mi raccontò una strana storia...\n\n Mi disse che un giorno lei, era bruco, era nata in un angolino, dentro una strana gabbia con dei misteriosi vetri trasparenti al posto delle sbarre.\nTutti i giorni questo posto veniva frequentato regolarmente da questi strani ingegneri, loro la chiamavano aula...\n C'era una persona in particolare più anziana delle altre, sembrava essere il loro capo e voleva che gli altri gli portassero rispetto, anche se non sempre ci riusciva. Alcuni pretendevano di essere autorevoli, ma tranne qualche eccezione, non accorgendosi della contraddizione finivano con essere solo autoritari.\nTutti loro si facevano chiamare professori, ma alcuni non erano capaci di insegnare le loro conoscenze ...mentre altri erano davvero dei maestri del sapere!!\nQuesta persona tracciava degli strani segni su una superficie nera, e parlava ...e tutti gli altri lo stavano ad ascoltare ricopiando quegli strani disegni, alcuni con aria assonnata, altri pieni di dubbi.\n\nPer fortuna un giorno lei si addormentò e quando si svegliò gli spuntarono due stupende ali variopinte che le permisero di volare via dalla sua gabbia.\nEra libera! Finalmente libera!\nEd era felice! Finalmente felice, di potersene stare in quel bel giardino all'aria aperta.\nFinalmente poteva girare in tutte le direzioni senza farsi influenzare da chi le diceva di scegliere la via di destra, sinistra o dritto al centro... se solo voleva poteva tornarsene indietro infischiandosene di tutto!! \nMa il fatto che tutti quegli ingegneri se ne stessero lì dentro la turbava un po'.\n\n A quel punto mi regalò la sua idea: \"Perchè non ricopi anche tu quegli strani disegni? Fai in modo che tutti possano copiarseli con poca fatica, così quei poveri ingegneri se ne potranno andare prima dalla loro gabbia e potranno essere come me che volo felice per questo giardino\".\n\nAllora nel colpo di un clock misi in opera la sua idea, con l'aiuto di alcuni amici molto bravi e generosi, ricopiai quegli strani disegni in un luogo straordinario, raggiungibile da tutto il mondo e dove ognuno può farsene una copia con un click!\n\nEbbi la possibilità di parlare con alcuni di questi ingegneri, anche loro erano tutti contenti e felici, finalmente avevano a disposizione un po' di aiuto per sollevarli dal loro carico... non capii perché, ma qualcuno mi disse che ora poteva volare.\n\n In una strana giornata, piovigginava leggermente con il sole che ancora faceva capolino tra le nubi, tornai nel giardino per ringraziare la mia amica farfalla... girai a destra e a manca, provai a chiamarla, ma mi accorsi che non sapevo nemmeno il suo nome.\n...non riuscii a trovarla, e stanco mi rimisi a sedere sulla panchina.\nPerò dopo un po' mi si presentò una simpatica rondinella, mi disse: \"Erano giorni che avevo una fame terribile... sai, vengo da un lungo viaggio. Per fortuna che è passata da questa parti una farfalla che era tanto felice e faceva tanto chiasso che non ho potuto non notarla... davvero un buon pasto!\".\n");
+		$template->assign('manifesto_Author', 'Brain');
 
 		
 		$template->display('manifesto.tpl');
