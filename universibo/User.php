8654aa16d4596d0959205ed59cf265591d88b6b2
Ilias Bartolini <ibartoli@thoughtworks.com>
2003-09-06 10:43:57 +0000

    aggiunte funzioni isValidPassword() e isValidUsername()

--- universibo/User.php
+++ universibo/User.php
@@ -1,5 +1,14 @@
 <?php
 
+define('USER_OSPITE',0);
+define('USER_STUDENTE',1);
+define('USER_MODERATORE',2);
+define('USER_TUTOR',4);
+define('USER_DOCENTE',8);
+define('USER_PERSONALE',16);
+define('USER_ADMIN',32);
+
+
 /**
  * User class
  *
@@ -23,11 +32,83 @@ class User {
 		
 
 		
-				
+		
 	}
 	
 	
+	/**
+	 *  Verifica se la sintassi dello username ט valido.
+	 *  Sono permessi fino a 25 caratteri: alfanumerici, lettere accentate, spazi, punti, underscore
+	 *
+	 * @param string $username stringa dello username da verificare
+	 * @return bool
+	 */
+	function isUsernameValid( $username )
+	{
 
+		$username_pattern='^([[:alnum:]אטעילש ._]{1,25})$';
+		return ereg($username_pattern , $username );
+	
+	}
+	
+
+	/**
+	 *  Verifica se la sintassi della password ט valida.
+	 *  Lunghezza min 5, max 30 caratteri
+	 *
+	 * @static
+	 * @param string $password stringa della password da verificare
+	 * @return bool
+	 */
+	function isPasswordValid( $password )
+	{
+		//$password_pattern='^([[:alnum:]]{5,30})$';
+		//ereg($password_pattern , $password );
+		$length = strlen( $password );
+		return ( $lenght > 5 && $length < 30 );
+	
+	}
+	
+	
+	
+	/**
+	 *  Verifica se la sintassi della password ט valida.
+	 *  Lunghezza min 5, max 30 caratteri
+	 *
+	 * @static
+	 * @param string $password stringa della password da verificare
+	 * @return bool
+	 */
+/*	function isPasswordValid( $password )
+	{
+		//$password_pattern='^([[:alnum:]]{5,30})$';
+		//ereg($password_pattern , $password );
+		$length = strlen( $password );
+		return ( $lenght > 5 && $length < 30 );
+	
+	}
+*/	
+	
+	
+	/**
+	 *  Verifica se la sintassi della password ט valida.
+	 *  Lunghezza min 5, max 30 caratteri
+	 *
+	 * @static
+	 * @param string $password stringa della password da verificare
+	 * @return bool
+	 */
+/*	function isPasswordValid( $password )
+	{
+		//$password_pattern='^([[:alnum:]]{5,30})$';
+		//ereg($password_pattern , $password );
+		$length = strlen( $password );
+		return ( $lenght > 5 && $length < 30 );
+	
+	}
+*/	
+	
+	
 }
 
 ?>
