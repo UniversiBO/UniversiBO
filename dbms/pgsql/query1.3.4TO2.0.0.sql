9877af6181e9acf4ad9d2352b43142777c476ca2
Ilias Bartolini <ibartoli@thoughtworks.com>
2003-09-08 19:55:29 +0000

    aggiunte query modifica tabella utente

--- dbms/pgsql/query1.3.4TO2.0.0.sql
+++ dbms/pgsql/query1.3.4TO2.0.0.sql
@@ -1,3 +1,4 @@
+#-- modifiche phpbb 2.0.6
 CREATE TABLE phpbb_confirm (
     confirm_id character(32) DEFAULT '' NOT NULL,
     session_id character(32) DEFAULT '' NOT NULL,
@@ -10,4 +11,15 @@ ALTER TABLE ONLY phpbb_confirm
 INSERT INTO "phpbb_config" ("config_name", "config_value") VALUES ('enable_confirm', '0');
 INSERT INTO "phpbb_config" ("config_name", "config_value") VALUES ('sendmail_fix', '0');
 UPDATE "phpbb_config" SET config_value = '.0.6' WHERE config_name='version';
----------
+#-- modifiche livello -> groups
+ALTER TABLE "utente" ADD "groups" int4 ;
+
+UPDATE "utente" SET "groups" = 2 WHERE "livello" = 100;
+UPDATE "utente" SET "groups" = 4 WHERE "livello" = 200;
+UPDATE "utente" SET "groups" = 8 WHERE "livello" = 300;
+UPDATE "utente" SET "groups" = 16 WHERE "livello" = 400;
+UPDATE "utente" SET "groups" = 32 WHERE "livello" = 600;
+UPDATE "utente" SET "groups" = 64 WHERE "livello" = 500;
+
+ALTER TABLE "utente" DROP COLUMN "livello";
+#--
