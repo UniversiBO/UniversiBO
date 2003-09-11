3e8ebb1e11c7f42068e7ef7bee587fd0e98a4e85
Ilias Bartolini <ibartoli@thoughtworks.com>
2003-09-11 20:30:42 +0000

    Scrittura comando ShowHome: mostra la homepage

---
+++ universibo/commands/ShowHome.php
@@ -0,0 +1,28 @@
+<?php
+
+include ('UniversiboCommand'.PHP_EXTENSION);
+
+/**
+ * ShowHome: mostra la homepage
+ *
+ * @package universibo
+ * @version 2.0.0
+ * @author Ilias Bartolini <brain79@inwind.it>
+ * @license GPL, {@link http://www.opensource.org/licenses/gpl-license.php}
+ */
+
+class ShowHome extends UniversiboCommand 
+{
+
+	function execute()
+	{
+		$template =& $this->frontController->getTemplateEngine();
+	
+		$template->assign('home_langWelcome', 'Benvenuto in UniversiBO!');
+		$template->assign('home_langWhatIs', 'Questo è il nuovo portale per la didattica, dedicato agli studenti dell\'università di Bologna.');
+		$template->assign('home_langMission', 'L\'obiettivo verso cui è tracciata la rotta delle iniziative e dei servizi che trovate su questo portale è di "aiutare gli studenti ad aiutarsi tra loro", fornirgli un punto di riferimento centralizzato in cui prelevare tutte le informazioni didattiche riguardanti i propri corsi di studio e offrire un mezzo di interazione semplice e veloce con i docenti che partecipano all\'iniziativa.');
+	}
+
+}
+
+?>
