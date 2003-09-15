170f21617f968b762c0f878b03471388bf8f65a2
Fabrizio Pinto <evaimitico@gmail.com>
2003-09-15 09:23:50 +0000

    mostra la pagina collabora

---
+++ universibo/commands/ShowContribute.php
@@ -0,0 +1,31 @@
+<?php
+
+include ('UniversiboCommand'.PHP_EXTENSION);
+
+/**
+ * ShowContributes is an extension of UniversiboCommand class.
+ *
+ * It shows Contribute page
+ *
+ * @package universibo
+ * @version 2.0.0
+ * @author Fabrizio Pinto
+ * @author Ilias Bartolini <brain79@inwind.it>
+ * @license GPL, {@link http://www.opensource.org/licenses/gpl-license.php}
+ */
+ 
+class ShowContributes extends UniversiboCommand {
+	function execute(){
+
+		$template =& $this->frontController->getTemplateEngine();
+		
+		
+		
+		
+		
+		
+						
+		}
+}
+
+?>
