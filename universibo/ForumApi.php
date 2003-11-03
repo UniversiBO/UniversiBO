a5175eb1210613970631c7fc979b04e7228e6c77
Ilias Bartolini <ibartoli@thoughtworks.com>
2003-11-03 10:26:06 +0000

    Aggiunta API esterna per il forum

---
+++ universibo/ForumApi.php
@@ -0,0 +1,34 @@
+<?php
+
+
+/**
+ * La classe Forum fornisce un'API esterna per le operazioni sul forum PHPBB 2.0.x
+ * 
+ * Rispettando le interfacce messe a disposizione si possono creare
+ * API per altri tipi di forum.
+ *
+ * @package universibo
+ * @version 2.0.0
+ * @author Ilias Bartolini <brain79@virgilio.it>
+ * @license GPL, <{@link http://www.opensource.org/licenses/gpl-license.php}>
+ * @copyright CopyLeft UniversiBO 2001-2003
+ */
+
+class Forum 
+{
+	
+	/**
+	 * @access private
+	 */
+	var $database = 'main';
+	
+	login()
+	{
+		
+	}
+	
+}
+
+
+
+?>
