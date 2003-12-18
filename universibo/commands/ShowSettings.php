c3f505a2315fd82ae6f88fc78a3ee02d22285edd
Fabrizio Pinto <evaimitico@gmail.com>
2003-12-18 19:39:58 +0000

    comando che apre la pagina impostazioni

---
+++ universibo/commands/ShowSettings.php
@@ -0,0 +1,35 @@
+<?php
+
+include ('UniversiboCommand'.PHP_EXTENSION);
+
+/**
+ * ShowRules is an extension of UniversiboCommand class.
+ *
+ * It shows rules page
+ *
+ * @package universibo
+ * @subpackage commands
+ * @version 2.0.0
+ * @author Fabrizio Pinto
+ * @author Ilias Bartolini <brain79@virgilio.it>
+ * @license GPL, {@link http://www.opensource.org/licenses/gpl-license.php}
+ */
+ 
+class ShowSettings extends UniversiboCommand {
+	function execute(){
+
+		$template =& $this->frontController->getTemplateEngine();
+
+		$template->assign('mypage_intro','Ora ti trovi nella tua pagina personale.<br />
+Tramite questa pagina potrai modificare il tuo profilo, le tue impostazioni personali ed
+avere un accesso veloce e personalizzato alle informazioni scegliendo i contenuti 
+e il loro formato tramite le tue 
+<font class="NormalC">Preferences</font>.');
+		$template->assign('mypage_preferences',array('pw'=>'Modifica password', 'info'=>'Informazioni personali', 'setting'=>'Impostazioni personali','mail'=>'Posta di ateneo'));
+		$template->assign('mypage_admin',array('postgre'=>'DB Postgresql locale', 'oracle'=>'DB Oracle ateneo', 'mysql'=>'DB MySql facoltà', 'nuovi'=>'Iscrivi nuovo utente'));
+		
+		return 'default';						
+	}
+}
+
+?>
