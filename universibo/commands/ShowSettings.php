ec8c0776935b84270c1e6d4b21bd285bfbea9588
Ilias Bartolini <ibartoli@thoughtworks.com>
2003-12-19 09:53:16 +0000

    pagina impostazioni

--- universibo/commands/ShowSettings.php
+++ universibo/commands/ShowSettings.php
@@ -18,7 +18,19 @@ include ('UniversiboCommand'.PHP_EXTENSION);
 class ShowSettings extends UniversiboCommand {
 	function execute(){
 
-		$template =& $this->frontController->getTemplateEngine();
+		$fc =& $this->getFrontController() 
+		$template =& $fc->getTemplateEngine();
+		$utente =& $fc->getSessionUser();
+
+
+		if ($utente->isAdmin())
+		{
+			$template->assign('mypage_showAdminPanel','true');
+		}
+		else
+		{
+			$template->assign('mypage_showAdminPanel','false');
+		}
 
 		$template->assign('mypage_intro','Ora ti trovi nella tua pagina personale.<br />
 Tramite questa pagina potrai modificare il tuo profilo, le tue impostazioni personali ed
@@ -27,7 +39,6 @@ e il loro formato tramite le tue
 <font class="NormalC">Preferences</font>.');
 		$template->assign('mypage_preferences',array('pw'=>'Modifica password', 'info'=>'Informazioni personali', 'setting'=>'Impostazioni personali','mail'=>'Posta di ateneo'));
 		$template->assign('mypage_admin',array('postgre'=>'DB Postgresql locale', 'oracle'=>'DB Oracle ateneo', 'mysql'=>'DB MySql facoltà', 'nuovi'=>'Iscrivi nuovo utente'));
-		
 		return 'default';						
 	}
 }
