01cdff560ecb5749ac53dba4f3f762036266c310
Ilias Bartolini <ibartoli@thoughtworks.com>
2004-09-07 17:42:29 +0000

    *** empty log message ***

--- universibo/commands/ScriptUpdateFileHash.php
+++ universibo/commands/ScriptUpdateFileHash.php
@@ -20,13 +20,37 @@ class ScriptUpdateFileHash extends UniversiboCommand
 	function execute()
 	{
 		$fc =& $this->getFrontController();
-		$template =& $this->frontController->getTemplateEngine();
+		$template =& $fc->getTemplateEngine();
+		$db =& $fc->getDbConnection('main');
 		$user =& $this->getSessionUser();
+		$filePath = $fc->getAppSetting('filesPath');
 		
 		if(!$user->isAdmin())
 			Error::throw(_ERROR_DEFAULT,array('msg'=>'La modifica della password non può essere eseguita da utenti con livello ospite.'."\n".'La sessione potrebbe essere scaduta, eseguire il login','file'=>__FILE__,'line'=>__LINE__));
 		
+		$res =& $db->query('SELECT id_file, nome_file FROM file ORDER BY 1');
 		
+		while ( $res->fetchInto($row) )
+		{
+			$nome_file = $filePath.$row[0].'_'.$row[1];
+			if (file_exists($nome_file)) 
+			{
+				$query = 'UPDATE file SET hash_file=\''.md5_file($nome_file).'\' WHERE id_file = '.$row[0];
+				$res1 =& $db->query($query);
+				if (DB::isError($res1)) 
+					Error::throw(_ERROR_CRITICAL,array('msg'=>DB::errorMessage($res1),'file'=>__FILE__,'line'=>__LINE__)); 
+			}
+			else  
+			{
+				echo $row[0].'_'.$row[1]."\n";
+				$query = 'UPDATE file SET hash_file=\'\' WHERE id_file = '.$row[0];
+				$res1 =& $db->query($query);
+				if (DB::isError($res1)) 
+					Error::throw(_ERROR_CRITICAL,array('msg'=>DB::errorMessage($res1),'file'=>__FILE__,'line'=>__LINE__)); 
+			}
+		}
+		
+		$res->free();
 		
 	}
 }
