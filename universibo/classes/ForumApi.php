c9404b939c31573320e41bfe41c6d502e3addcc6
Fabrizio Pinto <evaimitico@gmail.com>
2005-04-22 17:56:38 +0000

    query per ultimi dal forum senza limit

--- universibo/classes/ForumApi.php
+++ universibo/classes/ForumApi.php
@@ -482,19 +482,19 @@ class ForumApi
 		
 		$db =& FrontController::getDbConnection($this->database);
 		
+		// @NB se si usa limitQuery() la query non deve avere ';' alla fine
 		$query = 'SELECT t.topic_title, min(p.post_id) FROM '.$this->table_prefix.'posts p, '.$this->table_prefix.'topics t 
 					WHERE t.topic_id = p.topic_id 
 					AND p.forum_id = '.$db->quote($id_forum).'	
 					AND p.post_id IN (SELECT pp.post_id FROM '.$this->table_prefix.'posts pp WHERE t.topic_id = pp.topic_id AND pp.post_time > '.$user->getUltimoLogin().' ORDER BY pp.post_time ASC)
 					GROUP BY t.topic_title
-					ORDER BY max(p.post_id) DESC LIMIT 10;';
+					ORDER BY max(p.post_id) DESC';
 		
-		//var_dump($query);
+		//var_dump($db);
 		//
 		// volevo usare limitQuery e non il limit sql ma da errore
-		//$res =& $db->limitQuery($query, 0 , $num);
+		$res =& $db->limitQuery($query, 0 , $num);
 		
-		$res =& $db->query($query);
 		
 		if (DB::isError($res)) 
 			Error::throwError(_ERROR_DEFAULT,array('msg'=>DB::errorMessage($res),'file'=>__FILE__,'line'=>__LINE__)); 
