b29a2537d1d3e257fdf7d3c04373bc7ebd137574
Roberto Floris <roby46@gmail.com>
2005-05-26 12:36:16 +0000

    *** empty log message ***

--- dbms/pgsql/query2.0.0to2.1.0.sql
+++ dbms/pgsql/query2.0.0to2.1.0.sql
@@ -51,6 +51,61 @@ CREATE TABLE file_studente_commenti (
     PRIMARY KEY (id_commento),
     UNIQUE (id_file,id_utente,id_commento)
 );
+--query help--
+SELECT setval('help_id_help_seq', (SELECT MAX(id_help) FROM help) +1);
+
+INSERT INTO help (titolo,contenuto,ultima_modifica,indice) VALUES ('Cos\'è il servizio files studenti?
+','
+E\' un servizio che dà la possibilità di inserire dei files (appunti, lucidi, ecc...) relativi ad un insegnamento, a tutti gli studenti registrati. Non è da confondere con il servizio che permette al professore l\'inserimento di un file nel proprio insegnamento: i file inseriti dal professore appaiono sempre in basso alla pagina dell\'insegnamento, mentre i file inseriti dagli studenti sono visualizzati nel box “Contributi degli studenti” sulla colonna destra della pagina.',1117109715,310);
+
+INSERT INTO help (titolo,contenuto,ultima_modifica,indice) VALUES ('Come faccio ad inserire un file studenti?
+','
+Il procedimento per caricare un file sul sito è questo:
+[list=1][*][url=index.php?do=ShowHelp#id10]Accedere al sito col proprio nome utente[/url] (username).
+[*][url=index.php?do=ShowHelp#id3]Andare nella pagina[/url] in cui si desidera inserire il file.
+[*]Cliccare la voce "Inserisci il tuo contributo" nel nel box "Contributi degli studenti", apparirà una nuova pagina "Aggiungi un nuovo file" nella quale dovrai completare i seguenti campi:
+[list][*][b]File[/b]:  inserire il percorso interno al tuo computer del file che vuoi caricare, puoi aiutarti con il pulsante "Sfoglia" (o \'Browse\') e cercare la cartella del pc in cui é contenuto il file, selezionarlo e cliccare su \'Apri\' (o \'Open\', a seconda del sistema operativo utilizzato).  Per rendere piú veloce il processo di upload (e conseguentemente di download da parte degli utenti che desidereranno scaricare il file sul loro computer) é consigliabile comprimere il file prima di inviarlo.
+NB: si puó caricare [b]un solo file alla volta[/b], quindi per mettere on line piú files bisogna ripetere la procedura.
+[*][b]Titolo:[/b] inserire un titolo significativo per il file, in modo che si intuisca facilmente il contenuto. Il titolo comparirà nella lista dei file inseriti dagli studenti
+[*][b]Descrizione:[/b] scrivere una descrizione esauriente del contenuto del file.
+[*][b]Parole chiave:[/b] scrivere al massimo 4 parole, separate da un Enter/Invio, per consentire una più facile individuazione del file da parte di altri utenti, mediante l\'uso di un motore di ricerca file.
+[*][b]Categoria:[/b] scegliere la tipologia del file che stai inserendo tramite il menù a discesa. Se i file non appartiene a nessuna delle categorie presenti, lasciare "altro".
+[*][b]Data e ora d\'inserimento.[/b] questi campi vengono completati automaticamente; se lo si desidera, li si può cambiare: può essere molto utile nel caso in cui si voglia che il file non sia visibile prima di una certa data e ora.
+[*][b]Permessi download:[/b]  selezionando dal menù a discesa la voce "Solo iscritti", il file sarà scaricabile dai soli utenti registrati, mentre selezionando "Tutti", il file sarà scaricabile da chiunque fruisce del sito.
+[*][b]Il file verrà inserito nella pagina :[/b] qui viene visualizzato il nome dell\'insegnamento/corso in cui il file verrà inserito, il contenuto non è modificabile e viene impostato in automatico
+[/list]
+[*]Cliccare su invia.
+[/list]Se la procedura é stata completata con successo, verrà visualizzata una pagina di conferma del  corretto inserimento del file. Cliccando sul link "Torna a..." si ritorna alla pagina dove é stata inserito il file. Il titolo del nuovo file inserito comparirà nel Box \'Contributi degli studenti\'.
+',1117109715,320);
+
+INSERT INTO help (titolo,contenuto,ultima_modifica,indice) VALUES ('Come faccio a modificare un file studenti?','
+La modifica di un file studenti già presente può essere eseguita esclusivamente dall\'autore o dai referenti dell\'insegnamento.
+Se si è in possesso dei diritti per effettuare questa operazione, bisogna cliccare sul nome del file che si vuole modificare all\'interno del Box \'Contributi degli studenti\'; si passerà a una pagina con le informazioni relative al file, per effettuare le modifiche  é sufficiente cliccare sul pulsante \'Modifica\' [img]https://www.universibo.unibo.it/tpl/unibo/file_edit_32.gif[/img]  
+Verrà visualizzata la pagina di modifica analoga a quella di inserimento del file i cui campi contengono le informazioni del precedente inserimento. Effettuate le modifiche necessarie, é sufficiente cliccare sul pulsante \'Invia\' in basso alla pagina.
+Se la procedura é stata completata con successo, verrà visualizzata una pagina di conferma della corretta modifica del file. Per tornare ai dettagli del file cliccare sul link \'torna ai dettagli del file\'
+',1117109715,330);
+
+INSERT INTO help (titolo,contenuto,ultima_modifica,indice) VALUES ('Come faccio ad eliminare un file studenti?
+','
+Come per la modifica, l\'eliminazione di un file studenti può essere eseguita esclusivamente dall\'autore o dai referenti dell\'insegnamento.
+Se si è in possesso dei diritti per effettuare questa operazione, bisogna cliccare sul nome del file che si vuole eliminare all\'interno del Box \'Contributi degli studenti\'; si passerà a una pagina con le informazioni relative al file, per effettuare l\'eliminazione cliccare sul pulsante \'Elimina\'[img]https://www.universibo.unibo.it/tpl/unibo/file_del_32.gif[/img]. Verrà presentata una pagina di conferma, se si é sicuri di volere cancellare il file cliccare ancora su \'Elimina\'.
+',1117109715,340);
+
+INSERT INTO help (titolo,contenuto,ultima_modifica,indice) VALUES ('Cosa sono i commenti ai file studenti?
+','
+Per ogni file studente inserito è possibile scrivere un proprio commento. Ogni utente può aggiungere un solo commento per ogni file e deve esprimere un giudizio con un voto da zero a cinque.
+',1117109715,350);
+
+INSERT INTO help (titolo,contenuto,ultima_modifica,indice) VALUES ('Come posso aggiungere un commento ad un file studenti?
+','
+Il procedimento per inserire un commento è il seguente:
+[list=1][*]Cliccate sul titolo del file nel box “contributi degli studenti”, si passerà ad un\'altra pagina con le informazioni relative al file, in basso a questa pagina nel box Commenti cliccate sulla voce “Aggiungi il tuo commento!”; verrà presentata una nuova pagina nella quale si dovranno completare i seguenti campi:
+[list][*][b]Il tuo commento/descrizione sul file[/b]: scrivere una propria opinione sul file.
+[*][b]Il tuo voto[/b]: utilizzando il menù a discesa, assegnate un voto al file da 0 a 5.
+[*]infine cliccare su "INVIA".
+[/list]
+[/list]
+',1117109715,360);
 
 
 
