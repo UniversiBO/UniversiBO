e3f7187f85dabcec4f0d6051298418e05ca3f1d2
Daniele Tiles <daniele.tiles@gmail.com>
2006-02-03 20:17:01 +0000

    LastHope: questa query mancava sul cvs, ma potrebbe non esservi necessaria...

--- dbms/pgsql/query2.2.0to2.4.0.sql
+++ dbms/pgsql/query2.2.0to2.4.0.sql
@@ -7,3 +7,15 @@ ALTER TABLE "info_didattica" ALTER COLUMN "orario_ics_link" SET NOT NULL;
 
 --evaimitico 22/09/2005
 UPDATE canale SET links_attivo = 'S' WHERE id_canale = '25'
+
+--03-02-2005 LastHope
+--Questa query non esiste sul CVS, ma puo' essere che il vostro db sia gia' sistemato...vedete voi
+CREATE TABLE docente_contatti (
+    cod_doc character varying(6) NOT NULL,
+    stato integer DEFAULT 1 NOT NULL,
+    id_utente_assegnato integer,
+    ultima_modifica integer,
+    report text DEFAULT ''::text NOT NULL
+);
+ALTER TABLE ONLY docente_contatti
+    ADD CONSTRAINT docente_contatti_pkey PRIMARY KEY (cod_doc);
