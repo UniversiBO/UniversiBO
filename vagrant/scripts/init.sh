a279d465703878a18472ada6685ebdcb5bddf8ab
Davide Bellettini <davide@bellettini.me>
2014-11-22 17:45:02 +0100

    Fixing librarian-puppet issues

--- vagrant/scripts/init.sh
+++ vagrant/scripts/init.sh
@@ -13,7 +13,7 @@ fi
 cp /vagrant/vagrant/puppet/Puppetfile $PUPPET_DIR
 
 if [ `gem list | grep librarian | wc -l` -eq 0 ]; then
-  gem install librarian-puppet
+  gem install puppet librarian-puppet
   cd $PUPPET_DIR && librarian-puppet install --clean
 else
   cd $PUPPET_DIR && librarian-puppet update
