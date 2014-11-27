772184e34e2fbcf30dc23d9808445633142b0191
Davide Bellettini <davide@bellettini.me>
2014-11-28 00:11:48 +0100

    Apache now runs as vagrant

--- vagrant/puppet/modules/lapp/manifests/config.pp
+++ vagrant/puppet/modules/lapp/manifests/config.pp
@@ -1,54 +1,50 @@
 class lapp::config
 {
-    include postgresql::server
+  include postgresql::server
 
-    class {'apache':
-        default_vhost => false,
-        mpm_module => 'prefork'
-    }
+  class {'apache':
+    default_vhost => false,
+    mpm_module    => 'prefork',
+    user          => 'vagrant',
+    group         => 'vagrant',
+  }
 
-    apache::mod { 'rpaf': }
-    apache::mod { 'rewrite': }
-    class {'::apache::mod::php':
-    }
+  apache::mod { 'rpaf': }
+  apache::mod { 'rewrite': }
+  class {'::apache::mod::php':
+  }
 
-    postgresql::server::db { 'universibo':
-        user     => 'universibo',
-        password => 'universibo'
-    }
+  postgresql::server::db { 'universibo':
+    user     => 'universibo',
+    password => 'universibo'
+  }
 
-    postgresql::server::db { 'universibo_forum3':
-        user     => 'universibo',
-        password => 'universibo'
-    }
+  postgresql::server::db { 'universibo_forum3':
+    user     => 'universibo',
+    password => 'universibo'
+  }
 
-    file { '/etc/apache2/conf.d/user':
-        content => "User vagrant\nGroup vagrant"
-    }
+  file { '/etc/apache2/conf.d/user':
+    content => "User vagrant\nGroup vagrant"
+  }
 
+  exec { 'ports.conf':
+    command => 'sed \'/^NameVirtualHost/d\' /etc/apache2/ports.conf'
+  }
 
-#    exec { 'allow-all':
-#        command => "sed 's/.*allow from 127.*/Allow from All/i' -i /etc/apache2/conf.d/phppgadmin"
-#    }
+  exec { 'reload':
+    command => 'apache2ctl graceful',
+    require => Exec['ports.conf']
+  }
 
-    exec { 'ports.conf':
-        command => "sed '/^NameVirtualHost/d' /etc/apache2/ports.conf"
-    }
-
-    exec { 'reload':
-        command => 'apache2ctl graceful',
-#        require => Exec['allow-all', 'ports.conf', 'enable-modules']
-        require => Exec['ports.conf']
-    }
-
-    apache::vhost { 'default-universibo':
-        priority        => '10',
-        vhost_name      => '*',
-        port            => '80',
-        docroot         => '/vagrant/web',
-        docroot_owner   => 'vagrant',
-        docroot_group   => 'vagrant',
-        logroot         => '/var/log',
-        override        => 'All'
-    }
+  apache::vhost { 'default-universibo':
+    priority        => '10',
+    vhost_name      => '*',
+    port            => '80',
+    docroot         => '/vagrant/web',
+    docroot_owner   => 'vagrant',
+    docroot_group   => 'vagrant',
+    logroot         => '/var/log',
+    override        => 'All'
+  }
 }
