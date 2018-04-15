33ca446ca9773c8da8889adafe52955f404f64bd
Davide Bellettini <davide@bellettini.eu>
2018-04-15 18:38:24 +0000

    Use APP_ENV to get the environment

--- web/index.php
+++ web/index.php
@@ -7,7 +7,7 @@ $loader = require_once __DIR__.'/../app/bootstrap.php.cache';
 
 require_once __DIR__.'/../app/AppKernel.php';
 
-$env = getenv('SYMFONY_ENV') ?: 'dev';
+$env = getenv('APP_ENV') ?: 'dev';
 
 $kernel = new AppKernel('dev', $env === 'dev');
 $kernel->loadClassCache();
